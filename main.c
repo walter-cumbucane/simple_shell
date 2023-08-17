5ee94f6f5d1978fbab1826cfae7e30e7b83a83fd
walter-cumbucane <cumbucane.walter@gmail.com>
2023-08-17 20:04:03 +0200

    Task 1 done

---
+++ main.c
@@ -0,0 +1,47 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+
+/**
+ * main - super simple shell, taking commands and only exits on EOF or Ctrl-D
+ * @ac: argument count
+ * @av: argument vector
+ * @env: environment variable
+ *
+ * Return: Always zero on success, else exits on execution errro
+ */
+int main(int ac, char **av, char **env)
+{
+	char *line, *args[2];
+	size_t len = 0;
+	int exec, status;
+	pid_t pid;
+
+	while (1) /* looping the prompt */
+	{
+		if (isatty(STDIN_FILENO))
+			write(stdout, "$ ", 2);
+		if (getline(&line, &len, stdin) == -1)
+			exit(98);
+		strtok(line, "\n");
+		args[0] = line;
+		args[1] = NULL;
+		pid = fork();
+		if (pid == 0)
+		{
+			exec = execve(args[0], args, env); /* calling for execution of command/s */
+			if (exec == -1)
+			{
+				write(stdout, "./shell: No such file or directory\n", 35);
+				exit(98);
+			}
+		}
+		else if (pid > 0)
+			wait(&status);
+		else
+			perror("Error:");
+	}
+	free(line);
+	return (0);
+}
